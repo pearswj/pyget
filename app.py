78f19cdeda0da35319507c52f1c1fcc5c3a9abe6
Will Pearson <pearswj@gmail.com>
2014-12-18 16:30:51 +0000

    connect from anywhere

--- app.py
+++ app.py
@@ -353,4 +353,4 @@ def ping():
     return "pong"
 
 if __name__ == "__main__":
-    app.run(port=int(os.environ.get('FLASK_PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('FLASK_PORT', 5000)))
